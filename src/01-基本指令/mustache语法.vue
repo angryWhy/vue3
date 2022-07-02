c18df10b8514eadde00d5a4b548a85b3de9fee4e
angryWhy <875978822@qq.com>
2022-07-02 15:12:12 +0800

    Mustache语法

---
+++ src/01-基本指令/mustache语法.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+    <!-- 字符反转> -->
+    <h2>{{ message.split(" ").reverse().join(" ") }}</h2>
+    <!-- 表达式 -->
+    <h2>{{ count * 10 + 2 }}</h2>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  name: "MustaChe",
+  setup() {
+    const message = ref("Hello world");
+    const count = ref(0);
+    return {
+      message,
+      count,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
