4f5e064f38ce03e1a3d38e67e86d0664d39e3748
angryWhy <875978822@qq.com>
2022-07-08 22:11:18 +0800

    day-05-引用元素

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 <script>
 import { defineComponent } from "vue";
-import Demo from "./引用元素和组件/father.vue";
+import Demo from "./引用元素/father.vue";
 export default defineComponent({
   components: {
     Demo,
---
+++ src/引用元素/father.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <son-cpn ref="title"></son-cpn>
+    <h2 ref="child">原生元素</h2>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import SonCpn from "./son.vue";
+
+export default defineComponent({
+  name: "FatherCpn",
+  components: {
+    SonCpn,
+  },
+  methods: {
+    btnClick() {
+      //proxy对象
+      this.$$refs.title;
+      //原生元素
+      this.$$refs.child;
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
